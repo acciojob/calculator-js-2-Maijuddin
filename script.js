7c9cfe78466fbe823b33362befa705e978cbc05f
Maijuddin Mallick <124431155+Maijuddin@users.noreply.github.com>
2023-07-03 20:32:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function addToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function clearOneCharacter() {
+    var displayValue = document.getElementById("display").value;
+    document.getElementById("display").value = displayValue.slice(0, -1);
+}
+
+function calculate() {
+    var displayValue = document.getElementById("display").value;
+    var result = eval(displayValue);
+    document.getElementById("display").value = result;
+}
